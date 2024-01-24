04d13ccb5a5619583f989667cd996ededafcc5e7
lionel <ndabagajeanlionel@gmail.com>
2024-01-24 20:38:59 +0200

    Adding a person working well

--- home/views.py
+++ home/views.py
@@ -7,3 +7,16 @@ def home(request):
         "records" : Person.objects.all()
     }
     return render(request, 'index.html', context)
+
+
+def add(request):
+    if request.method == "POST":
+        person = Person()
+        person.first_name = request.POST["first_name"]
+        person.last_name = request.POST["last_name"]
+        person.age = request.POST["age"]
+        person.occupation = request.POST["occupation"]
+        person.location = request.POST["location"]
+        person.save()
+
+    return render(request, 'addperson.html')
